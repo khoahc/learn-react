5c5115b5e7437c5ee69ad2bd8e58352f829557d4
Dang Khoa <58696742+khoahc@users.noreply.github.com>
2021-12-29 08:50:31 -0500

    (397) - Js Expressions in JSX Practice

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./styles.css";
 
+const name = "Khoa";
+const currentYear = new Date().getFullYear();
 export default function App() {
   return (
     <div className="App">
-      <h1>My Favourite Foods</h1>
-      <ul>
-        <li>Noodles</li>
-        <li>Cake</li>
-        <li>Bacon</li>
-      </ul>
+      <h1>Created by {name}</h1>
+      <h1>Copyright {currentYear}</h1>
     </div>
   );
 }
