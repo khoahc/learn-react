bdb5e4ec14a495d288d6bfa817582326cbf081ee
Dang Khoa <58696742+khoahc@users.noreply.github.com>
2021-12-30 11:56:45 -0500

    400. React Styling Practice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import "./styles.css";
 
-const name = "Khoa";
-const currentYear = new Date().getFullYear();
+const date = new Date();
+let currentHour = date.getHours();
+let greeting;
+const customStyle = {
+  color: ""
+};
+
+if (currentHour < 12) {
+  greeting = "Good morning";
+  customStyle.color = "red";
+} else if (currentHour < 18) {
+  greeting = "Good afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good night";
+  customStyle.color = "blue";
+}
+
 export default function App() {
   return (
-    <div className="App">
-      <h1>Created by {name}</h1>
-      <h1>Copyright {currentYear}</h1>
+    <div>
+      <h1 className="heading" style={customStyle}>
+        {greeting}
+      </h1>
     </div>
   );
 }
